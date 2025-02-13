998d568e48eeec567f2bc2ad8e92f34289db4400
Zann181 <sazambranoc@unal.edu.co>
2025-02-13 10:23:28 -0500

    teclado_buffer

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "keypad.h" 
+//#include "ring_buffer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -56,56 +57,22 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+uint32_t key_pressed_tick = 0;
+uint16_t column_pressed = 0;
 
+uint32_t debounce_tick = 0;
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-  static uint32_t last_interrupt_time = 0;
-  if (HAL_GetTick() - last_interrupt_time < 100) {
+  if ((debounce_tick + 200) > HAL_GetTick()) {
     return;
   }
-  last_interrupt_time = HAL_GetTick();
-  switch (GPIO_Pin)
-  {
-    case COLUMN_1_Pin:
-      if (HAL_GPIO_ReadPin(COLUMN_1_GPIO_Port, COLUMN_1_Pin) == GPIO_PIN_SET) {
-        HAL_UART_Transmit(&huart2, (uint8_t *)"E", 1, 10);
-        return; // Error si el boton no esta presionado, no procesar
-      }
-      HAL_GPIO_WritePin(ROW_1_GPIO_Port, ROW_1_Pin, GPIO_PIN_SET);
-      HAL_Delay(2);
-      if (HAL_GPIO_ReadPin(COLUMN_1_GPIO_Port, COLUMN_1_Pin) == GPIO_PIN_SET) {
-        HAL_UART_Transmit(&huart2, (uint8_t *)"1", 1, 10); // Columna 1, Fila 1
-        break; 
-      }
-      HAL_GPIO_WritePin(ROW_2_GPIO_Port, ROW_2_Pin, GPIO_PIN_SET);
-      HAL_Delay(2);
-      if (HAL_GPIO_ReadPin(COLUMN_1_GPIO_Port, COLUMN_1_Pin) == GPIO_PIN_SET) {
-        HAL_UART_Transmit(&huart2, (uint8_t *)"4", 1, 10); // Columna 1, Fila 2
-        break;
-      }
-      HAL_GPIO_WritePin(ROW_3_GPIO_Port, ROW_3_Pin, GPIO_PIN_SET);
-      HAL_Delay(2);
-      if (HAL_GPIO_ReadPin(COLUMN_1_GPIO_Port, COLUMN_1_Pin) == GPIO_PIN_SET) {
-        HAL_UART_Transmit(&huart2, (uint8_t *)"7", 1, 10); // Columna 1, Fila 3
-        break;
-      }
-      HAL_GPIO_WritePin(ROW_4_GPIO_Port, ROW_4_Pin, GPIO_PIN_SET);
-      HAL_Delay(2);
-      if (HAL_GPIO_ReadPin(COLUMN_1_GPIO_Port, COLUMN_1_Pin) == GPIO_PIN_SET) {
-        HAL_UART_Transmit(&huart2, (uint8_t *)"*", 1, 10); // Columna 1, Fila 4
-        break;
-      }
-      break;
-    
-    default:
-      break;
-  }
-  HAL_GPIO_WritePin(ROW_1_GPIO_Port, ROW_1_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(ROW_2_GPIO_Port, ROW_2_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(ROW_3_GPIO_Port, ROW_3_Pin, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(ROW_4_GPIO_Port, ROW_4_Pin, GPIO_PIN_RESET);
+  debounce_tick = HAL_GetTick();
+  key_pressed_tick = HAL_GetTick();
+  column_pressed = GPIO_Pin;
 }
 /* USER CODE END 0 */
+
+
 
 /**
   * @brief  The application entry point.
@@ -122,6 +89,7 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+
 
   /* USER CODE BEGIN Init */
 
@@ -143,8 +111,15 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  //ring_buffer_init(&rb, ring_buffer_memory, RB_CAPACITY);
+  keypad_init();
+  HAL_UART_Transmit(&huart2, (uint8_t *)"Hello World\n", 12, 100);
+  while (1) {
+    if (column_pressed != 0 && (key_pressed_tick + 5) < HAL_GetTick() ) {
+      uint8_t key = keypad_scan(column_pressed);
+      HAL_UART_Transmit(&huart2, &key, 1, 100);
+      column_pressed = 0;
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -294,21 +269,21 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = ROW_1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(ROW_1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : ROW_2_Pin ROW_4_Pin ROW_3_Pin */
   GPIO_InitStruct.Pin = ROW_2_Pin|ROW_4_Pin|ROW_3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 1, 0);
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 1, 0);
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
